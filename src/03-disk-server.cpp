70f4e30382d18cccfc714241a98f6740223c7545
jesus-r-mendoza <jesus_r_mendoza@yahoo.com>
2019-04-28 11:30:24 -0700

    moved info, read, write, and help funcs

--- src/03-disk-server.cpp
+++ src/03-disk-server.cpp
@@ -28,25 +28,51 @@ int blockIndex(int c, int s) {
     return ( c * SECTORS ) + s;
 }
 
+void info(vector<string> args) {
+    if ( args.size() != 1 )
+        cout << "ERR: Command \"I\" requires 0 arguments\n\n";
+    else {
+        cout << "+--------------+" << endl; 
+        cout << "| Disk Format: |" << endl;
+        cout << "|  " << CYLINDERS << " Cylinders |" << endl;
+        cout << "|  " << SECTORS << " Sectors   |" << endl;
+        cout << "+--------------+" << endl;
+    }
+}
+
+void read(vector<string> args) {
+    if ( args.size() != 3 ) 
+        cout << "ERR: Command \"R\" requires 2 arguments\n\n";
+    else
+        cout << "Executing: R " << args[1] << " " << args[2] << "\n\n";    
+}
+
+void write(vector<string> args) {
+    if ( args.size() != 5 ) 
+        cout << "ERR: Command \"W\" requires 4 arguments\n\n";
+    else
+        cout << "Executing: W " << args[1] << " " << args[2] << " " << args[3] << " " << args[4] << "\n\n";
+}
+
+void help() {
+    cout << "You asked for help\n\n";
+}
+
 bool processArgs(vector<string> args) {
     if ( args.size() == 0 )
         return false;
     if ( args[0] == "I" )
-        cout << "+--------------+\n| Disk Format: | \n|  3 Cylinders | \n|  8 Sectors   | \n+--------------+\n\n";
-    else if ( args[0] == "R" ) {
-        if ( args.size() != 3 ) 
-            cout << "ERR: Command \"R\" requires 2 arguments\n\n";
-        else
-            cout << "Executing: R " << args[1] << " " << args[2] << "\n\n";
-    } else if ( args[0] == "W" ) {
-        if ( args.size() != 4 ) 
-            cout << "ERR: Command \"W\" requires 3 arguments\n\n";
-        else
-            cout << "Executing: W " << args[1] << " " << args[2] << " " << args[3] << "\n\n";
-    } else if ( args[0] == "exit" ) {
+        info(args);
+    else if ( args[0] == "R" )
+        read(args);
+    else if ( args[0] == "W" )
+        write(args);
+    else if ( args[0] == "help" )
+        help();
+    else if ( args[0] == "exit" ) {
         cout << "\nExiting...\nBye\n\n";
         return true;
-    } else 
+    } else
         cout << "ERR: Command \"" << args[0] << "\" not found. Use command \"help\" for more info.\n\n";
     return false;
 }
