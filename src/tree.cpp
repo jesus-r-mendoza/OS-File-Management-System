73a886944f7a47898997e0a5059f379768e462a6
jesus-r-mendoza <jesus_r_mendoza@yahoo.com>
2019-05-02 19:43:36 -0700

    pwd and ls work

--- src/tree.cpp
+++ src/tree.cpp
@@ -6,8 +6,7 @@ using namespace std;
 
 class Node {
     public:
-    Node(string nombre, Node* par) {
-        parent = par;
+    Node(string nombre) {
         name = nombre;
         isDir = isDirectory();
     }
@@ -26,7 +25,7 @@ class Node {
         children.push_back(child);
         return true;
     }
-
+    
     bool delChild(Node* child) {
         if ( child == NULL )
             return false;
@@ -43,12 +42,12 @@ class Node {
         return false;
     }
 
-    Node* getChild(Node* child) {
-        if ( child == NULL )
+    Node* getChild(string child) {
+        if ( child.length() <= 0 )
             return NULL;
         for ( Node* x : children ) {
-            if ( x->name == child->name )
-                return child;
+            if ( x->name == child ) 
+                return x;
         }
         return NULL;
     }
@@ -68,32 +67,71 @@ class Tree {
     Node* current = root;
     string ls() {
         string list = "";
-	if(current->isDir && current->children.size() > 0) {
+	    if(current->isDir && current->children.size() > 0) {
             for ( Node* child : current->children )
                 list += "  " + (child->name);
-            cout << "has children\n";
         }
         return list;
     }
     
     string pwd() {
-        Node* par = current;
-        string path = "" + current->name;
-	while (	par != root && par != NULL ) {
-	    par = current->parent;
-	    path = par->name + path;
-	}
+        Node* curr = current;
+        Node* par = curr->parent;
+        string path = "" + curr->name;
+        while ( par != NULL ) {
+            curr = par;
+            path = curr->name + path;
+            par = curr->parent;
+        }
         return path;
+    }
+
+    void cd(string child) {
+        if ( child == ".." ) {
+            if ( current->parent != NULL ) 
+                current = current->parent;
+        } else {
+            Node* c = current->getChild(child);
+            if ( c != NULL && c->isDir )
+                current = c;
+        }
     }
 };
 
 int main() {
-    Node r("root/", NULL);
+    Node r("root/");
+    Node a("a");
+    Node b("b/");
+    Node c("c");
+    r.addChild(&a);
+    r.addChild(&b);
+    r.addChild(&c);
+
+    Node b1("b1/");
+    Node b2("b2");
+
+    b.addChild(&b1);
+    b.addChild(&b2);
+
     Tree tree(&r);
+    cout << "pwd() : " << tree.pwd() << endl;
+    cout << "ls() : " << tree.ls() << endl;
+    cout << "cd b/ \n";
+    tree.cd("b/");
+    cout << "pwd() : " << tree.pwd() << endl;
+    cout << "ls() : " << tree.ls() << endl;
+    tree.cd("b1/");
+    tree.cd("..");
+    tree.cd("b2");
+    //cout << "ls() : " << tree.pwd() << endl;
+    /*
     cout << tree.root->isDir << endl;
     cout << tree.current->name << endl;
     cout << tree.ls() << endl;
     cout << tree.pwd() << endl;
+    */
+
+
     /*
     cout << "- - - - - - - - - - - -\n";
     Node x;
