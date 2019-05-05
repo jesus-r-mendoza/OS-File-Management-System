92f9f399ba66d389cf798587027257d703586ebb
jesus-r-mendoza <jesus_r_mendoza@yahoo.com>
2019-05-05 16:59:15 -0700

    creating a file works

--- src/04-file-server.cpp
+++ src/04-file-server.cpp
@@ -11,7 +11,28 @@ using namespace std;
 #define SECTORS 8
 #define BLOCK_SIZE 128
 
-string clean() {
+const string indicator = "<~/#+?$=&>";
+
+vector<string> split(string str, string regex) {
+    int found;
+    vector<string> vect;
+    string segment;
+    while ( ( found = str.find(regex) ) != -1 ) {
+        segment = str.substr(0, found);
+        str.erase(0, found + regex.length());
+        if ( segment != " " && segment.length() > 0 )
+            vect.push_back(segment);
+    }
+    if ( str.length() > 0 )
+        vect.push_back(str);
+    return vect;
+}
+
+int blockIndex(int c, int s) {
+    return ( c * SECTORS ) + s;
+}
+
+string reset() {
     ofstream freeBlocks;
     freeBlocks.open("storage/free-blocks.dsk");
     if ( freeBlocks.fail() ) {
@@ -56,7 +77,45 @@ string getAvailBlock() {
     return avail;
 }
 
+string createFile(int fileID, string fileName) {
+    string avail = getAvailBlock();
+    if ( avail.length() == 0 )
+        return "[0] ERR: No more free space available.\n\n";
+    vector<string> coor = split(avail, ",");
+    string writeData = "";
+    for ( int i = 0; i < BLOCK_SIZE - indicator.length() - 3; i++ )
+        writeData += '0';
+    writeData += indicator + "eof";
+
+    // send() request to disk server
+    cout << writeData << endl;
+    cout << writeData.length() << endl;
+    // receive confirmation from disk server that data was written
+
+    ifstream fat;
+    fat.open("storage/fat.dsk");
+    if ( fat.fail() ) {
+        return "[0] ERR: Could not access \"storage/fat.dsk\"\n\n";
+    }
+    ofstream temp;
+    temp.open("storage/.temp.fat.dsk");
+    if ( temp.fail() ) {
+        return "[0] ERR: Could not access \"storage/.temp.fat.dsk\"\n\n";
+    }
+    string line;
+    while ( getline(fat, line) )
+        temp << line + '\n';
+    int block = blockIndex(stoi(coor[0]), stoi(coor[1]));
+    temp << fileID << ',' << fileName << ",1," << to_string(block) << ',' << avail << '\n';
+    fat.clear();
+    fat.close();
+    temp.clear();
+    temp.close();
+    remove("storage/fat.dsk");
+    rename("storage/.temp.fat.dsk", "storage/fat.dsk");
+}
+
 int main() {
-    cout << clean();
-    cout << getAvailBlock();
+    cout << reset();
+    createFile(12, "a.txt");
 }
