e22b387a462aeebd0565fe6be4acbd3b58a238c3
jesus-r-mendoza <jesus_r_mendoza@yahoo.com>
2019-05-05 18:22:20 -0700

    list function work ; need to work on write func

--- src/04-file-server.cpp
+++ src/04-file-server.cpp
@@ -77,6 +77,18 @@ string getAvailBlock() {
     return avail;
 }
 
+string addAvailBlock(string avail) {
+    ofstream freeBlocks;
+    freeBlocks.open("storage/free-blocks.dsk", ios::app);
+    if ( freeBlocks.fail() ) {
+        return "[0] ERR: Could not access \"storage/free-blocks.dsk\"\n\n";
+    }
+    freeBlocks << avail + '\n';
+    freeBlocks.clear();
+    freeBlocks.close();
+    return "[1] Successfully added free block.\n\n";
+}
+
 string createFile(int fileID, string fileName) {
     string avail = getAvailBlock();
     if ( avail.length() == 0 )
@@ -101,9 +113,86 @@ string createFile(int fileID, string fileName) {
     fat << fileID << ',' << fileName << ",1," << to_string(block) << ',' << avail << '\n';
     fat.clear();
     fat.close();
+    return "[1] Successfully created a new file.\n\n";
+}
+
+string deleteFile(int fileID) {
+    string id = to_string(fileID);
+    ifstream fat;
+    fat.open("storage/fat.dsk");
+    if ( fat.fail() ) {
+        return "[0] ERR: Could not access \"storage/fat.dsk\"\n\n";
+    }
+    string line;
+
+    fstream temp;
+    temp.open("storage/.temp.fat.dsk");
+    if ( temp.fail() ) {
+        return "[0] ERR: Could not access \"storage/.temp.fat.dsk\"\n\n";
+    }
+    vector<string> parts;
+    string cyl = "-1";
+    string sec = "-1";
+    while ( getline(fat, line) ) {
+        parts = split(line, ",");
+        if ( id == parts[0] ) {
+            cyl = parts[4];
+            sec = parts[5];
+        } else
+            temp << line + '\n';
+    }
+    if ( cyl == "-1" && sec == "-1" ) {
+        fat.clear();
+        fat.close();
+        temp.clear();
+        temp.close();
+        return "[0] ERR: This file doesn't exist. Cannot be deleted.\n\n";
+    } 
+    addAvailBlock(cyl + ',' + sec);
+    // send "R" request to disk server with cyl and sec that was gathered
+    // receive confirm notice that block exits
+    // capture string that holds the file's data
+    string data = "";
+    parts = split(data, indicator);
+    vector<string> nums;
+    while ( parts[1] != "eof" ) {
+        nums = split(parts[1], ",");
+        cyl = nums[0];
+        sec = nums[1];
+        // send "R" request to disk server with cyl and sec that was gathered
+        // receive confirm notice that block exits
+        // capture string that holds the file's data
+        data = ""; // whatever was received from read request
+        addAvailBlock(cyl + ',' + sec);
+        parts = split(data, indicator);
+    }
+    return "[1] Successfully delete the file. And allocated all of its blocks as free.\n\n";
+}
+
+string list(string flag) {
+    if ( flag != "0" && flag != "1" )
+        return "[0] ERR: Unrecognized flag.\n\n";
+    ifstream fat;
+    fat.open("storage/fat.dsk");
+    if ( fat.fail() ) {
+        return "[0] ERR: Could not access \"storage/fat.dsk\"\n\n";
+    }
+    string line;
+    string result = (flag == "0" ? "All files:\n" : "id\t{Name}\t(Blocks)\t<Index>\t[cyl]\t[sec]\n");
+    vector<string> parts;
+    while ( getline(fat, line) ) {
+        parts = split(line, ",");
+        if ( flag == "0" ) 
+            result += parts[1] + '\n';
+        else
+            result += parts[0] + "\t{" + parts[1] + "}\t(" + parts[2] + ")\t<" + parts[3] + ">\t[" + parts[4] + "]\t[" + parts[5] + "]\n";
+    }
+    result += '\n';
+    return result;
 }
 
 int main() {
     cout << reset();
     createFile(12, "a.txt");
+    cout << list("1");
 }
